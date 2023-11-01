f26f40672b899ba4f9e856e5f23a78b591433492
hftamayo <hftamayo@gmail.com>
2023-11-01 14:48:31 -0600

    updating root endpoint

--- routes/user.go
+++ routes/user.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/itsmaheshkariya/gin-gorm-rest/controller"
+)
 
 func UserRoute(router *gin.Engine){
-	router.GET("/", func(c *gin.Context){
-		c.String(200, "Hello World")
-	})
+	router.GET("/", controller.UserController)
 }
