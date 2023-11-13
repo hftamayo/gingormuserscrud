186c3bb8f999680102aae74286e484b69e837c9c
hftamayo <hftamayo@gmail.com>
2023-11-13 14:42:56 -0600

    adding methods for persistence

--- controller/user.go
+++ controller/user.go
@@ -1,8 +1,35 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/itsmaheshkariya/gin-gorm-rest/models"
+	"github.com/itsmaheshkariya/gin-gorm-rest/config"
+)
 
-func UserController(c *gin.Context) {
-	c.String(200, "Hello World")
 
+func GetUsers(c *gin.Context) {
+	users := []models.User{}
+	config.DB.Find(&users)
+	c.JSON(200, &users)
+}
+
+func CreateUser(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	config.DB.Create(&user)
+	c.JSON(200, &user)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", C.Param("id")).First(&user)
+	c.BindJSON(&user)
+	config.DB.Save(&user)
+	c.JSON(200, &user)
 }
