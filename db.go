0cdf53598b6f87239e91b6834d2a08d868e73048
hftamayo <hftamayo@gmail.com>
2023-11-13 14:43:11 -0600

    first version of db scaffold

--- db.go
+++ db.go
@@ -1,10 +1,17 @@
 package config
 
 import (
+	"github.com/itsmaheshkariya/gin-gorm-rest/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+var DB *form.DB
+
 func Connect() {
 	db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"), &gorm.Config{})
+	if err != nil {
+	}
+	db.AutoMigrate(&models.User{})
+	DB = db
 }
