58b32cc4abb820b826cfb669c7680b0985ba3048
hftamayo <hftamayo@gmail.com>
2023-11-13 14:43:28 -0600

    adding user's routes

--- routes/user.go
+++ routes/user.go
@@ -6,5 +6,8 @@ import (
 )
 
 func UserRoute(router *gin.Engine){
-	router.GET("/", controller.UserController)
+	router.GET("/view-users", controller.GetUsers)
+	router.POST("/add-user", controller.CreateUser)
+	router.DELETE("/delete-user/:id", controller.DeleteUser)
+	router.PUT("/update-user/:id", controller.UpdateUser)
 }
