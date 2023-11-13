5bcc0c9ce50dc96e53b4dd8f02187426e027fc02
hftamayo <hftamayo@gmail.com>
2023-11-13 14:44:12 -0600

    updating main class

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import "github.com/gin-gonic/gin"
 import "github.com/itsmaheshkariya/gin-gorm-rest/routes"
+import "github.com/itsmaheshkariya/gin-gorm-rest/config"
 
 func main() {
 	router:= gin.New()
+	config.Connect()
 	routes.UserRoute(router)
 	router.Run(":9004")
 }
