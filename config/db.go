b013fb84fe76191cae4dd8d4dd7029110a06eea7
hftamayo <hftamayo@gmail.com>
2023-11-17 15:11:00 -0600

    working on use env for db connection

--- config/db.go
+++ config/db.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"log"
+	"os"
+	"fmt"
+
 	"github.com/hftamayo/gingormrest/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -8,10 +12,17 @@ import (
 
 var DB *gorm.DB
 
-func Connect() {
-	db, err := gorm.Open(postgres.Open("postgres://gobank:gobank@localhost:5432/postgres?sslmode=disable"), &gorm.Config{})
+func Connect() (*gorm.DB, error) {
+	databaseURL := os.Getenv("DATABASE_URL")
+	databaseName := os.Getenv("DATABASE_NAME")
+	connectionString := fmt.Sprintf(databaseURL, databaseName)
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	//db, err := gorm.Open(postgres.Open("postgres://gobank:gobank@localhost:5432/postgres?sslmode=disable"), &gorm.Config{})
 	if err != nil {
+		log.Printf("Error connecting to the database.\n%v", err)
+		return nil, err
 	}
 	db.AutoMigrate(&models.User{})
 	DB = db
+	return db, nil
 }
