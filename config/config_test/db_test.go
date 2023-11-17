0a20c0f2dff82770e86aaa5dee8c4b32ef168338
hftamayo <hftamayo@gmail.com>
2023-11-17 13:23:06 -0600

    udpdates to unit test cases

--- config/config_test/db_test.go
+++ config/config_test/db_test.go
@@ -2,18 +2,24 @@ package config_test
 
 import (
 	"testing"
+
 	"github.com/hftamayo/gingormrest/config"
 	sqlmock "github.com/stretchr/testify/mock"
 )
 
 func TestConnect(t *testing.T) {
+	// Create a mock database object
 	dbMock := sqlmock.Open(`postgres`)
 
+	// Set expectations for the mock database object
 	dbMock.Expect().Ping().WillReturn(nil)
 
+	// Replace the global DB variable with the mock database object
 	config.DB = dbMock
 
+	// Call the Connect function
 	config.Connect()
 
+	// Verify that the mock database object was called
 	dbMock.AssertExpectations(t)
 }
